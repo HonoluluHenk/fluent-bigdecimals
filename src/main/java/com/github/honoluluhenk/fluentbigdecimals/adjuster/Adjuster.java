9f8fc6788bbfd749af57929a53bc63444dbe11c6
Christoph Linder <post@christoph-linder.ch>
2020-11-29 03:25:22 +0100

    added FIXME: Is Adjuster maybe just a Scaler?

--- src/main/java/com/github/honoluluhenk/fluentbigdecimals/adjuster/Adjuster.java
+++ src/main/java/com/github/honoluluhenk/fluentbigdecimals/adjuster/Adjuster.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.MathContext;
 
+// FIXME: might this just be a Scaler?
 public interface Adjuster extends Serializable {
     BigDecimal adjust(BigDecimal value);
 
