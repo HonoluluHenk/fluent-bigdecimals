b1b41f0ae09bf02ec6a8428649edd6abaa0ca3da
Christoph Linder <post@christoph-linder.ch>
2020-12-07 21:29:12 +0100

    first demos implemented

--- src/test/java/com/github/honoluluhenk/fluentbigdecimals/demo/DemoTest.java
+++ src/test/java/com/github/honoluluhenk/fluentbigdecimals/demo/DemoTest.java
@@ -7,15 +7,54 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.math.RoundingMode;
 
+import static java.math.RoundingMode.HALF_UP;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoTest {
 
-    public static MathContext DEFAULT_MATH_CONTEXT = new MathContext(7, RoundingMode.HALF_UP);
-    public static BigDecimalFactory DEFAULT = BigDecimalFactory.factory(DEFAULT_MATH_CONTEXT, new MaxPrecisionScaler());
-    public static BigDecimalFactory DATABASE = BigDecimalFactory.jpaBigDecimal();
+    public static final MathContext DEFAULT_MATH_CONTEXT = new MathContext(7, HALF_UP);
+    public static final BigDecimalFactory DEFAULT = BigDecimalFactory.factory(DEFAULT_MATH_CONTEXT, new MaxPrecisionScaler());
+
+    public static final MathContext DATABASE_MATH_CONTEXT = new MathContext(18, HALF_UP);
+    public static final int DATABASE_MAX_SCALE = 2;
+    public static final BigDecimalFactory DATABASE = BigDecimalFactory.jpaBigDecimal();
+
+    @Nested
+    class OldSchool {
+        @Test
+        void using_bigdcimals() {
+
+            var a = new BigDecimal("12.3456789");
+            assertThat(a).isEqualTo("12.3456789");
+
+            // explicit rounding
+            var b = a.round(DEFAULT_MATH_CONTEXT);
+            assertThat(b).isEqualTo("12.34568");
+
+            // some math operation
+            var c = b.add(new BigDecimal("54.555555"), DEFAULT_MATH_CONTEXT);
+            // intermediate result: 66.901235 and then rounded
+            assertThat(c).isEqualTo("66.90124");
+
+            // continue with different scaler
+            var x = c.round(DATABASE_MATH_CONTEXT) // remember: needs scaling
+                .setScale(DATABASE_MAX_SCALE, DATABASE_MATH_CONTEXT.getRoundingMode());
+
+            assertThat(x).isEqualTo("66.90");
+
+            var y = x.multiply(new BigDecimal("123.99999"), DATABASE_MATH_CONTEXT)
+                .setScale(DATABASE_MAX_SCALE, DATABASE_MATH_CONTEXT.getRoundingMode());
+            // intermediate result: 8295.599331
+            assertThat(y).isEqualTo("8295.60");
+
+            // finally...
+            var result = y;
+            assertEquals("8295.60", result.toPlainString());
+        }
+
+    }
 
     @Nested
     class ApiDemo {
@@ -25,25 +64,30 @@ public class DemoTest {
             // start from factory
             var a = DEFAULT.of("12.3456789");
             // please note: no implicit rounding on creation
-            assertEquals(new BigDecimal("12.3456789"), a.getValue());
+            assertThat(a.getValue())
+                .isEqualTo("12.3456789");
 
             // explicit rounding
             var b = a.round();
-            assertEquals(new BigDecimal("12.34568"), b.getValue());
+            assertThat(b.getValue())
+                .isEqualTo("12.34568");
 
             // some math operation
             var c = b.add(new BigDecimal("54.555555"));
             // intermediate result: 66.901235 and then rounded
-            assertEquals(new BigDecimal("66.90124"), c.getValue());
+            assertThat(c.getValue())
+                .isEqualTo("66.90124");
 
             // continue with different scaler
             var x = c.roundInto(DATABASE); // = 592.54 (database only allows two decimals
-            assertEquals(new BigDecimal("66.90"), x.getValue());
+            assertThat(x.getValue())
+                .isEqualTo("66.90");
 
             // still on the other scaler
             var y = x.multiply(new BigDecimal("123.99999"));
             // intermediate result: 8295.599331
-            assertEquals(new BigDecimal("8295.60"), y.getValue());
+            assertThat(y.getValue())
+                .isEqualTo("8295.60");
 
             // finally get the actual BigDecimal
             var result = y.getValue();
