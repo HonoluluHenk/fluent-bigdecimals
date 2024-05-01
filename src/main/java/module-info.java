6d4e65c6c47867fb46a4c0397efe8a379c8a5ce5
Christoph Linder <post@christoph-linder.ch>
2024-05-02 00:24:25 +0200

    fix(Java Modules): mark module dependency to checker-qual as transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ open module com.github.honoluluhenk.fluentbigdecimals {
     exports com.github.honoluluhenk.fluentbigdecimals;
     exports com.github.honoluluhenk.fluentbigdecimals.scaler;
 
-    requires org.checkerframework.checker.qual;
+    requires transitive org.checkerframework.checker.qual;
 
     requires static lombok;
     requires static com.github.spotbugs.annotations;
